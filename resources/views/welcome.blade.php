c4569496ab2679325e97b560414d6b37430221d6
Andrey Grytsenko <andreyapg@gmail.com>
2022-08-07 11:23:42 +0300

    Base 2022-08-07

---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+    <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+
+        <title>Laravel</title>
+
+        @vite('resources/css/app.css')
+    </head>
+    <body class="container mx-auto">
+    <h1 class="text-3xl text-pink-900 hover:text-amber-700">Hello</h1>
+    </body>
+</html>
