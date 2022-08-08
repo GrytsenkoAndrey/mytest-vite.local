c3ebfdd26fdfafc30ce43d2b449ce71bf06f2ba1
Andrey Grytsenko <andreyapg@gmail.com>
2022-08-08 09:02:02 +0300

    Fix problem with build styles 2022-08-08

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -6,7 +6,7 @@
 
         <title>Laravel</title>
 
-        @vite('resources/css/app.css')
+        @vite('resources/sass/app.scss')
     </head>
     <body class="container mx-auto">
     <h1 class="text-3xl text-pink-900 hover:text-amber-700">Hello</h1>
